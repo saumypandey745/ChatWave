d9b3023c65085a6d5b8f3076ccb1ba0dc068155b
Saumy Pandey <saumypandey745@gmail.com>
2026-08-21 12:01:16 +0530

    fix: add localhost:5174 to CORS allowed origins

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ const isAllowedOrigin = (origin) => {
   if (!origin) return true; // Non-browser clients (curl, Postman, server-to-server)
   const allowed = [
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://localhost:3000',
     'http://localhost:5000',
     process.env.CLIENT_URL,
